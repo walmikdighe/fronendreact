6a369ac059b4426fc5188444436259d38905395c
walmikdighe <valmikdighe17@gmail.com>
2026-07-31 23:56:40 +0530

    start with condtional rendering

--- src/main.jsx
+++ src/main.jsx
@@ -16,8 +16,9 @@ import { ParentComponent } from './components/context-api/parentcomponent.jsx'
 import { FakestoreIndex } from './components/fakestore/fakestore-index.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { ConditionDemo } from './components/condition-demo/condition-demo.jsx'
 
 createRoot(document.getElementById('root')).render(
-<FakestoreIndex/>
+<ConditionDemo/>
 
 )
